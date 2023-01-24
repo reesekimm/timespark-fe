4216bc25126caa9df187594c1391972aa49c0bfd
reesekimm <reesekimm@gmail.com>
2023-01-24 21:44:44 +0900

    [#2] storybook config 파일 형식 변경 (.cjs -> .ts) 및 불필요한 파일 삭제

--- packages/ui/.storybook/main.ts
+++ packages/ui/.storybook/main.ts
@@ -1,4 +1,6 @@
-module.exports = {
+import type { StorybookConfig } from "@storybook/react-vite";
+
+const config: StorybookConfig = {
   stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
   addons: [
     "@storybook/addon-links",
@@ -13,3 +15,5 @@ module.exports = {
     storyStoreV7: true,
   },
 };
+
+export default config;
