30f780723ade9d7d2958910054c2f609e3b1c9fc
reesekimm <reesekimm@gmail.com>
2023-02-07 21:31:27 +0900

    [#11] Header, Footer story 타이틀 수정

--- packages/components/src/Footer/footer.stories.tsx
+++ packages/components/src/Footer/footer.stories.tsx
@@ -2,7 +2,7 @@ import type { Meta, StoryObj } from '@storybook/react'
 import { Footer } from './footer'
 
 const meta: Meta<typeof Footer> = {
-  title: 'Footer/Default',
+  title: 'Footer',
   component: Footer
 }
 
--- packages/components/src/Header/header.stories.tsx
+++ packages/components/src/Header/header.stories.tsx
@@ -2,7 +2,7 @@ import type { Meta, StoryObj } from '@storybook/react'
 import { Header } from './header'
 
 const meta: Meta<typeof Header> = {
-  title: 'Header/Default',
+  title: 'Header',
   component: Header
 }
 
