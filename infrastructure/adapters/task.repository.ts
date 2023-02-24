7e64c1d556f7e2e9d30b57cfc3cb0c748c075b4b
reesekimm <reesekimm@gmail.com>
2023-02-24 14:34:42 +0900

    httpAxios 리팩터링
    - 객체 -> 함수로 변경하여 중복 제거
    - params, data 등을 options 인자의 필드로 전달하도록 변경하여 가독성 개선

--- infrastructure/adapters/task.repository.ts
+++ infrastructure/adapters/task.repository.ts
@@ -1,20 +1,18 @@
-import { Task } from '@timespark/domain/models'
 import {
   CreateTaskDto,
   DeleteTaskDto,
   GetTasksDto,
   TaskRepository
 } from '@timespark/domain/repositories'
-import { Http } from '../types'
 import { httpAxios } from '../utils/http'
 
-const client: Http = httpAxios
+const client = httpAxios
 
 export const taskRepository: TaskRepository = {
   createTask: async (taskData: CreateTaskDto) =>
-    await client.post<boolean>('/task', { taskData }),
+    await client('/task', { data: taskData }),
   getTasks: async ({ from, to }: GetTasksDto) =>
-    await client.get<Task[]>('/tasks', { from, to }),
+    await client('/tasks', { params: { from, to } }),
   deleteTask: async ({ id }: DeleteTaskDto) =>
-    await client.delete<boolean>(`/task/${id}`)
+    await client(`/task/${id}`, { method: 'DELETE' })
 }
--- infrastructure/types.ts
+++ infrastructure/types.ts
@@ -1,23 +1,3 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
-export type Http = {
-  get: <T>(
-    path: string,
-    params?: Record<string, unknown>,
-    config?: any
-  ) => Promise<T | any>
-  post: <T>(
-    path: string,
-    params?: Record<string, unknown>,
-    config?: any
-  ) => Promise<T | any>
-  put: <T>(
-    path: string,
-    params?: Record<string, unknown>,
-    config?: any
-  ) => Promise<T | any>
-  delete: <T>(path: string, params?: unknown, config?: any) => Promise<T | any>
-}
-
 export interface CustomError extends Error {
   status?: number
   code?: string
