8a411c11e235d8b9ec38ec80677e3add951ac791
reesekimm <reesekimm@gmail.com>
2023-02-27 20:45:24 +0900

    폰트 파일을 정적으로 서빙하도록 변경
    문제: 태스크 시작시 폰트 swap 현상 발생
    
    원인: 태스크 시작시 매 초 setQueryData 실행 -> QueryClientProvider에 의해 App 리렌더링 -> 페이지에서 사용된 폰트들 fetch 요청 (GlobalStyle에 명시한 @font-face url() 호출 -> FOUT 발생
    
    해결:
    1. 폰트를 정적으로 서빙하도록 변경 (CDN 사용X)
        - packages/component: /public/fonts 경로에 폰트 파일 추가하고 storybook preview-head.html에서 @font-face url에 해당 경로 설정
        - views/web: /public/fonts 경로에 폰트 파일 추가하고 index.html에서 @font-face url에 해당 경로 설정
    2. packages/style의 global style에서 @font-face 속성 제거 및 font asset 제거

--- packages/styles/src/globalStyle.tsx
+++ packages/styles/src/globalStyle.tsx
@@ -1,39 +1,8 @@
 import { createGlobalStyle } from 'styled-components'
 import { normalize } from 'styled-normalize'
-import NanumSquareNeoLight from '../fonts/NanumSquareNeo-light.woff2'
-import NanumSquareNeo from '../fonts/NanumSquareNeo-regular.woff2'
-import NanumSquareNeoBold from '../fonts/NanumSquareNeo-bold.woff2'
-import NanumSquareNeoExtraBold from '../fonts/NanumSquareNeo-extrabold.woff2'
-import NanumSquareNeoHeavy from '../fonts/NanumSquareNeo-heavy.woff2'
 
 const GlobalStyle = createGlobalStyle`
   ${normalize}
-
-  @font-face {
-    font-family: 'NanumSquareNeoLight';
-    src: url(${NanumSquareNeoLight}) format('woff2');
-    font-weight: 100;
-  }
-  @font-face {
-    font-family: 'NanumSquareNeo';
-    src: url(${NanumSquareNeo}) format('woff2');
-    font-weight: 200;
-  }
-  @font-face {
-    font-family: 'NanumSquareNeoBold';
-    src: url(${NanumSquareNeoBold}) format('woff2');
-    font-weight: 500;
-  }
-  @font-face {
-    font-family: 'NanumSquareNeoExtraBold';
-    src: url(${NanumSquareNeoExtraBold}) format('woff2');
-    font-weight: 600;
-  }
-  @font-face {
-    font-family: 'NanumSquareNeoHeavy';
-    src: url(${NanumSquareNeoHeavy}) format('woff2');
-    font-weight: 700;
-  }
 
   html {
     font-size: 10px;
