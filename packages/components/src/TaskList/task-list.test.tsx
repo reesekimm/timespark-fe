e8094c6c9d87817e5cc0221d9aa7581d0973fcc1
reesekimm <reesekimm@gmail.com>
2023-03-02 22:59:39 +0900

    [#30] TaskList 컴포넌트 - 태스크 정지, 완료 테스트 코드 추가

--- packages/components/src/TaskList/task-list.test.tsx
+++ packages/components/src/TaskList/task-list.test.tsx
@@ -7,8 +7,13 @@ import { data } from './data'
 function renderTaskList({
   onDrop,
   onDelete,
-  onStart
-}: Pick<TaskListProps, 'onDrop' | 'onDelete' | 'onStart'>) {
+  onStart,
+  onPause,
+  onComplete
+}: Pick<
+  TaskListProps,
+  'onDrop' | 'onDelete' | 'onStart' | 'onPause' | 'onComplete'
+>) {
   render(
     <DndProvider backend={HTML5Backend}>
       <TaskList
@@ -16,6 +21,8 @@ function renderTaskList({
         onDrop={onDrop}
         onDelete={onDelete}
         onStart={onStart}
+        onPause={onPause}
+        onComplete={onComplete}
         activeTaskId={0}
       />
     </DndProvider>
@@ -26,15 +33,19 @@ describe('TaskList', () => {
   const onDrop = jest.fn()
   const onDelete = jest.fn()
   const onStart = jest.fn()
+  const onPause = jest.fn()
+  const onComplete = jest.fn()
 
   beforeEach(() => {
     onDrop.mockClear()
     onDelete.mockClear()
     onStart.mockClear()
+    onPause.mockClear()
+    onComplete.mockClear()
   })
 
   it('renders appropriately', () => {
-    renderTaskList({ onDrop, onDelete, onStart })
+    renderTaskList({ onDrop, onDelete, onStart, onPause, onComplete })
 
     const task = screen.getByRole('columnheader', { name: /task/i })
     const estimatedDuration = screen.getByRole('columnheader', {
@@ -50,26 +61,54 @@ describe('TaskList', () => {
 
     const tableBody = screen.getByRole('table').querySelector('tbody')
 
-    expect(tableBody?.children.length).toBe(5)
+    expect(tableBody?.children.length).toBe(7)
   })
 
   it('can delete task on click delete button', async () => {
-    renderTaskList({ onDrop, onDelete, onStart })
+    renderTaskList({ onDrop, onDelete, onStart, onPause, onComplete })
 
-    const deleteButtonOfFirstTask = screen.getAllByTitle('delete')[0]
+    const firstTaskDeleteButton = screen.getAllByTitle(/delete/i)[0]
 
-    await userEvent.click(deleteButtonOfFirstTask)
+    await userEvent.click(firstTaskDeleteButton)
 
     expect(onDelete).toBeCalled()
   })
 
   it('can start task', async () => {
-    renderTaskList({ onDrop, onDelete, onStart })
+    renderTaskList({ onDrop, onDelete, onStart, onPause, onComplete })
 
-    const startButtonOfFirstTask = screen.getAllByTitle('start')[0]
+    const firstTaskStartButton = screen.getAllByTitle(/start/i)[0]
 
-    await userEvent.click(startButtonOfFirstTask)
+    await userEvent.click(firstTaskStartButton)
 
     expect(onStart).toBeCalled()
+  })
+
+  it('can pause task', async () => {
+    renderTaskList({ onDrop, onDelete, onStart, onPause, onComplete })
+
+    const firstTaskStartButton = screen.getAllByTitle(/start/i)[0]
+    await userEvent.click(firstTaskStartButton)
+
+    const pauseButtons = await screen.findAllByTitle(/pause/i)
+    await userEvent.click(pauseButtons[0])
+
+    expect(onPause).toBeCalled()
+  })
+
+  it('can complete task', async () => {
+    renderTaskList({ onDrop, onDelete, onStart, onPause, onComplete })
+
+    const firstTaskStartButton = screen.getAllByTitle(/start/i)[0]
+
+    await userEvent.click(firstTaskStartButton)
+
+    const pauseButtons = await screen.findAllByTitle(/pause/i)
+    await userEvent.click(pauseButtons[0])
+
+    const completeButtons = await screen.findAllByTitle(/complete/i)
+    await userEvent.click(completeButtons[0])
+
+    expect(onComplete).toBeCalled()
   })
 })
