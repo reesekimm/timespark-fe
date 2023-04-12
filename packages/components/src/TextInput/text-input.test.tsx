e36a555b7b48d7ddd1306b61b8d7ebf5b7da5f29
reesekimm <reesekimm@gmail.com>
2023-04-12 14:30:06 +0900

    태스크 / 카테고리 입력 가능한 글자수 표시 (#53)

--- packages/components/src/TextInput/text-input.test.tsx
+++ packages/components/src/TextInput/text-input.test.tsx
@@ -3,7 +3,7 @@ import { TextInput } from './text-input'
 
 describe('TextInput', () => {
   it('renders text that user types', async () => {
-    render(<TextInput />)
+    render(<TextInput value='' onChange={jest.fn()} />)
 
     const input = screen.getByRole('textbox')
 
@@ -12,5 +12,21 @@ describe('TextInput', () => {
     await userEvent.type(input, 'hello world')
 
     expect(input).toHaveValue('hello world')
+  })
+
+  it('when maxLength is set, renders remaining number of characters', async () => {
+    render(<TextInput value='' onChange={jest.fn()} maxLength={5} />)
+
+    const input = screen.getByRole('textbox')
+    const remainingBefore = screen.queryByTestId('remaining')
+
+    expect(remainingBefore).not.toBeInTheDocument()
+
+    await userEvent.type(input, 'hello')
+
+    const remainingAfter = screen.getByTestId('remaining')
+
+    expect(remainingAfter).toBeInTheDocument()
+    expect(remainingAfter).toHaveTextContent('0')
   })
 })
--- packages/components/src/TextInput/text-input.tsx
+++ packages/components/src/TextInput/text-input.tsx
@@ -3,7 +3,9 @@ import {
   ChangeEventHandler,
   forwardRef,
   InputHTMLAttributes,
-  useMemo
+  useCallback,
+  useEffect,
+  useState
 } from 'react'
 import { v4 as uuidv4 } from 'uuid'
 import styled, { css } from 'styled-components'
@@ -12,47 +14,71 @@ import { Label } from '../Label/label'
 type InputSize = 'small' | 'medium' | 'large'
 
 export type Props = InputHTMLAttributes<HTMLInputElement> & {
+  value: string
+  onChange: ChangeEventHandler<HTMLInputElement>
   label?: string
   name?: string
-  value?: string
   placeholder?: string
   inputSize?: InputSize
-  onChange?: ChangeEventHandler<HTMLInputElement>
+  maxLength?: number
 }
 
 export const TextInput = forwardRef<HTMLInputElement, Props>(function TextInput(
-  { label, placeholder, inputSize = 'medium', onChange, style, ...rest }: Props,
+  {
+    label,
+    placeholder,
+    value,
+    inputSize = 'medium',
+    onChange,
+    maxLength,
+    style,
+    ...rest
+  }: Props,
   ref
 ) {
   const id = uuidv4()
 
-  const handleChange = useMemo(
-    () =>
-      onChange
-        ? (event: ChangeEvent<HTMLInputElement>) => {
-            if (onChange) {
-              onChange(event)
-            }
-          }
-        : undefined,
-    [onChange]
+  const [currValue, setCurrValue] = useState(value)
+
+  useEffect(() => {
+    setCurrValue(value)
+  }, [value])
+
+  const handleChange = useCallback(
+    (e: ChangeEvent<HTMLInputElement>) => {
+      onChange(e)
+      setCurrValue(
+        maxLength ? e.target.value.slice(0, maxLength) : e.target.value
+      )
+    },
+    [maxLength, onChange]
   )
 
   return (
-    <div style={style}>
+    <Wrapper style={style}>
       {label ? <Label htmlFor={id} label={label} size={inputSize} /> : null}
       <StyledInput
         id={id}
         ref={ref}
         type='text'
+        value={currValue}
         placeholder={placeholder}
         inputSize={inputSize}
         onChange={handleChange}
         {...rest}
       />
-    </div>
+      {maxLength && currValue.length >= 1 ? (
+        <Remaining size={inputSize} data-testid='remaining'>
+          {currValue.length > maxLength ? 0 : maxLength - currValue.length}
+        </Remaining>
+      ) : null}
+    </Wrapper>
   )
 })
+
+const Wrapper = styled.div`
+  position: relative;
+`
 
 const StyledInput = styled.input<Props>`
   display: block;
@@ -98,5 +124,40 @@ export const styleMap = {
     &:focus {
       box-shadow: ${({ theme }) => `0 0 0 3.6px ${theme.palette.primary}`};
     }
+  `
+}
+
+type RemainingProps = { size: InputSize }
+
+const Remaining = styled.div<RemainingProps>`
+  position: absolute;
+  ${({ size }) => size && remainingStyleMap[size]};
+  text-align: center;
+  background-color: ${({ theme }) => theme.palette.primary};
+  border-radius: 7px;
+  color: ${({ theme }) => theme.palette.white};
+  opacity: 0.6;
+`
+
+const remainingStyleMap = {
+  small: css`
+    right: 0.4rem;
+    bottom: 0.4rem;
+    width: 3rem;
+    padding: 0.6rem;
+    font-size: ${({ theme }) => theme.fontSize.xsmall};
+  `,
+  medium: css`
+    right: 0.6rem;
+    bottom: 0.6rem;
+    width: 3.6rem;
+    padding: 0.7rem;
+  `,
+  large: css`
+    right: 0.8rem;
+    bottom: 0.7rem;
+    width: 4.2rem;
+    padding: 0.8rem;
+    font-size: ${({ theme }) => theme.fontSize.medium};
   `
 }
